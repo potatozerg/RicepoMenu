e5242c909fa2d2368bab7e3d4de8539752e315bd
god830 <yukun_liu2009@hotmail.com>
2017-07-06 21:46:11 -0400

    all item logic done, working on price

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -10,7 +10,7 @@ app.controller('mainCtrl',function($scope){
 							{
 							  "name": "Add On",
 							  "min": 1,
-							  "max": 1,
+							  "max": 3,
 							  "items": [
 							    {
 							      "name": "bubble",
@@ -19,7 +19,7 @@ app.controller('mainCtrl',function($scope){
 							    },
 							    {
 							      "name": "pudding",
-							      "price": 50,
+							      "price": 80,
 							      "available": true
 							    }
 							  ]
@@ -44,7 +44,7 @@ app.controller('mainCtrl',function($scope){
 							{
 							  "name": "Size",
 							  "min": 1,
-							  "max": 1,
+							  "max": 3,
 							  "items": [
 							      {
 							        "name": "Large",
@@ -70,59 +70,113 @@ app.controller('mainCtrl',function($scope){
 	$scope.adjustedJSON = _.cloneDeep($scope.testJSON);
 	delete $scope.adjustedJSON["name"];
 	delete $scope.adjustedJSON["price"];
-	console.log($scope.adjustedJSON["options"][0].name);
-	$scope.count = [];
-	console.log($scope.adjustedJSON["options"].length);
-	console.log($scope.adjustedJSON["options"][0]["items"].length);
 
+	//initiate the 2d array that stores the count of each iteam
+	$scope.count = [];
 	_.times($scope.adjustedJSON["options"].length, function(i){
 		$scope.count[i] = [];
 	    _.times($scope.adjustedJSON["options"][i]["items"].length, function(j){
 	    	$scope.count[i][j] = 0;
 		});
 	});
-	//$scope.count[2][0]=4;
-	console.log($scope.testJSON.options[0].max);
 
-
+	//initiate the queue, each option has its own queue
 	$scope.queue = [];
-
 	_.times($scope.adjustedJSON["options"].length, function(i){
 		$scope.queue[i] = [];
 	});
-
-	$scope.addToQueue = function(index,item){
-
-		$scope.queue[index].push(item);
-		console.log($scope.queue[index]);
-	}
 	
+	//whether show the count on the right
 	$scope.showCount = function(parentIndex,index){
-		if($scope.count[parentIndex][index]>0){
+		if($scope.count[parentIndex][index] > 0){
 			return true;
 		}else{
 			return false;
 		}
 	}
 
-
+	//add function
 	$scope.add = function(parentIndex,index){
-		//console.log("clicked");
+
+		//get the privous total number of selections for an option
+		let previousCount = 0;
+		_.times($scope.count[parentIndex].length, function(i){
+			previousCount += $scope.count[parentIndex][i];			
+		});
+		//console.log(previousCount);
+		//if max == 0 there is no limit, just add
+		if($scope.adjustedJSON["options"][parentIndex]["max"] != 0){
+			//check if it's max count already
+			if(previousCount == $scope.adjustedJSON["options"][parentIndex]["max"]){
+
+				//find the first selected item, make its index --
+				let pop = $scope.queue[parentIndex].shift();
+				//add the new one to queue
+				let tempIndex = 0;
+				//find the popped item index
+				for (var i = 0; i < $scope.adjustedJSON["options"][parentIndex]["items"].length; i++) {
+					if($scope.adjustedJSON["options"][parentIndex]["items"][i]["name"] == pop){	
+						break;
+					}else{
+						tempIndex++;
+					}			
+				}
+				$scope.count[parentIndex][tempIndex]--;
+				$scope.price -= $scope.adjustedJSON["options"][parentIndex]["items"][tempIndex]["price"];
+				//console.log($scope.count[parentIndex][tempIndex]);	
+			}
+		}
+		//count it and add it to the queue
 		$scope.count[parentIndex][index]++;
-		//console.log($scope.count[0][0]);
 		$scope.addToQueue(parentIndex,$scope.adjustedJSON["options"][parentIndex]["items"][index]["name"]);
+		$scope.price += $scope.adjustedJSON["options"][parentIndex]["items"][index]["price"];
 	}
 
+	//add an item to the queue
+	$scope.addToQueue = function(parentIndex,item){
+
+		$scope.queue[parentIndex].push(item);
+		console.log($scope.queue[parentIndex]);
+	}
+
+	//remove function
 	$scope.remove = function(parentIndex,index){
-		//console.log("clicked");
+		
 		$scope.count[parentIndex][index]--;
 		//console.log($scope.count[0][0]);
+		//remove the item in the queue
+		$scope.removeFromQueue(parentIndex,$scope.adjustedJSON["options"][parentIndex]["items"][index]["name"]);
+		$scope.price -= $scope.adjustedJSON["options"][parentIndex]["items"][index]["price"];
 	}
 
+	//remove an item from the queue
+	$scope.removeFromQueue = function(parentIndex,item){
 
+		//console.log("item: "+item);
+		//find the last element for the item and remove it
+		let index = $scope.queue[parentIndex].lastIndexOf(item);
+		$scope.queue[parentIndex].splice(index,1);
+		console.log($scope.queue[parentIndex]);		
+	}
 
+	//function that triggers after the price is clicked
+	$scope.priceClicked = function(){
+
+		if(1){
+			$scope.priceArray = [];
+			console.log($scope.price/100);
+			_.times($scope.queue.length, function(i){
+				$scope.priceArray = $scope.priceArray.concat($scope.queue[i]);
+			});
+			console.log($scope.priceArray);
+
+		}else{
+
+		}
+	}
 });
 
+//filter for price
 app.filter('priceFilter', function() {
     return function(x) {
        return x/100;
